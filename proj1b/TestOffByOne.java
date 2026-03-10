ac6f1ce8acc211d59c9e47d3001479024f1afa41
xuzhipeng <xzp0630@gmail.com>
2026-03-11 01:24:27 +0800

    complete proj1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -13,6 +13,7 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('%', '&'));
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'B'));
     }
 
     // Your tests go here.
